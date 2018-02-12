425596e2776ecae257df01fa6e5f661218f4cda1
Your Name <nick.deboar@gmail.com>
2018-02-12 16:33:08 +1030

    readme and Deadline event

---
+++ Deadline/CustomEnvironmentCopy.py
@@ -0,0 +1,52 @@
+###############################################################
+# Imports
+###############################################################
+from System import *
+
+from Deadline.Events import *
+from Deadline.Scripting import *
+
+import os
+import string
+
+from ecosystem.environment import Environment as eco
+#########################################################################################
+# This is the function called by Deadline to get an instance of the Draft event listener.
+#########################################################################################
+
+
+def GetDeadlineEventListener():
+    return CustomEnvironmentCopyListener()
+
+
+def CleanupDeadlineEventListener(eventListener):
+    eventListener.Cleanup()
+
+###############################################################
+# The event listener class.
+###############################################################
+
+
+class CustomEnvironmentCopyListener (DeadlineEventListener):
+
+    def __init__(self):
+        self.OnJobSubmittedCallback += self.OnJobSubmitted
+
+    def Cleanup(self):
+        del self.OnJobSubmittedCallback
+
+    def OnJobSubmitted(self, job):
+		tools = ['houdini%s'%os.environ['HOUDINI_VERSION'], 'htoa%s'%os.environ['HTOA_VERSION']]
+		self.LogInfo("Ecosystem Tools: %s"%tools)
+		env = eco(tools)
+		wantedkeys = env.variables.keys()
+
+		self.LogInfo("On Job Submitted Event Plugin: Custom Environment Copy Started")
+
+		for key in wantedkeys:
+			self.LogInfo("Setting %s to %s" % (key, os.environ[key]))
+			job.SetJobEnvironmentKeyValue(key, os.environ[key])
+
+		RepositoryUtils.SaveJob(job)
+
+		self.LogInfo("On Job Submitted Event Plugin: Custom Environment Copy Finished")
--- Deadline/README.md
+++ Deadline/README.md
@@ -2,3 +2,7 @@
 A replacment for \\DeadlineRepository10\plugins\Houdini\Houdini.py
 
 The function RenderExecutable now looks to the HOUDINI_LOCATION env var for the location of Houdini; bypassing the UI. 
+
+# CustomEnvironmentCopy
+Add to the Deadline events directory.
+Currently just sets the environment variables for Houdini and HtoA that are defined by Ecosystem, and passes them onto Deadline. 
